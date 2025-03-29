93147d7e04cc67cd9092553cd2668a46c8579fa7
CryptoCodeMonkey <158448116+CryptoCodeMonkey@users.noreply.github.com>
2025-03-29 12:30:08 -0500

    Delete seasonal-solutions directory

--- seasonal-solutions/app/simple-page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SimplePage() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Seasonal Solutions</h1>
-      <p className="mt-4 text-xl">Handyman & Landscaping Services in Winnipeg</p>
-    </div>
-  )
-}
-
